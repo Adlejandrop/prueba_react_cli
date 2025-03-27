23c3cc0a70486278b0e16cd05166fb68ceee9c49
apinedo-sbpay <159149029+apinedo-sbpay@users.noreply-github.com>
2025-03-27 11:18:12 -0300

    [APINEDO]initial commit

---
+++ src/Theme.ts
@@ -0,0 +1,19 @@
+const Theme = {
+  padding: {
+    s: 16,
+    m: 24,
+    l: 48,
+  },
+  margin: {
+    s: 16,
+    m: 24,
+    l: 48,
+  },
+  imageSizes: {
+    s: {height: 100},
+    m: {height: 300},
+    l: {height: 450},
+  },
+};
+
+export default Theme;
